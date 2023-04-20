a0aad5fbd271006518e2a739d78eaf400f0a0320
RisingZero <ar.angelo259@gmail.com>
2023-04-20 16:54:42 +0200

    frontend/created root pages and horizontal menu

--- appy_frontend/src/app/app.component.ts
+++ appy_frontend/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { ComponentsModule } from './components/components.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [
+    IonicModule,
+    ComponentsModule
+  ],
 })
 export class AppComponent {
+
   constructor() {}
 }
--- appy_frontend/src/app/app.routes.ts
+++ appy_frontend/src/app/app.routes.ts
@@ -2,12 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'chat',
+    loadComponent: () => import('./chat/chat.page').then( m => m.ChatPage)
+  },
+  {
+    path: 'friends',
+    loadComponent: () => import('./friends/friends.page').then( m => m.FriendsPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./profile/profile.page').then( m => m.ProfilePage)
   },
 ];
